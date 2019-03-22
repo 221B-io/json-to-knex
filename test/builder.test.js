8e63e8d9bc7e09f5e33de7944623aa8092ae65d3
Cameron Blandford <cam.blandford@gmail.com>
2019-03-22 11:06:22 -0400

    Delete test database after tests are run

--- test/builder.test.js
+++ test/builder.test.js
@@ -1,7 +1,7 @@
 const builder = require("../index");
 const _ = require("lodash");
 const Knex = require("knex");
-
+const fs = require("fs");
 const schema = {
   tables: [
     {
@@ -116,12 +116,17 @@ const knexConfig = {
 };
 
 let knex;
+
 beforeAll(async () => {
   knex = Knex(knexConfig);
 });
 
 afterAll(async () => {
   knex.destroy();
+  fs.unlink("./test.sqlite3", err => {
+    if (err) throw err;
+    console.log("test database successfully deleted");
+  });
 });
 
 beforeEach(async () => {
