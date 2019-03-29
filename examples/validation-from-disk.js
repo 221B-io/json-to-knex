a37a49e2738b258ffe56678d930069490d86f1f8
Cameron Blandford <cam.blandford@gmail.com>
2019-03-29 14:14:36 -0400

    prettify validation example

--- examples/validation-from-disk.js
+++ examples/validation-from-disk.js
@@ -1,53 +1,50 @@
-const Ajv = require('ajv');
-const fse = require('fse');
-const path = require('path');
+const Ajv = require("ajv");
+const fse = require("fse");
+const path = require("path");
 
-const getSchema = (uri ) => {
-  return request.json(uri).then(function (res) {
+const getSchema = uri => {
+  return request.json(uri).then(function(res) {
     if (res.statusCode >= 400)
-      throw new Error('Loading error: ' + res.statusCode);
+      throw new Error("Loading error: " + res.statusCode);
     return res.body;
   });
-}
-
+};
 
 // const loadSchema = getSchema;
 
 async function go() {
-  const base = '../sql-json-schema/'
+  const base = "../sql-json-schema/";
 
-  const loadSchema = (uri) => {
+  const loadSchema = uri => {
     return fse.readJson(path.join(base, uri));
-  }
+  };
 
-  const schemaSchema = await fse.readJson(path.join(base, 'schema.schema.json'))
-  const ajv = new Ajv({ loadSchema, });
+  const schemaSchema = await fse.readJson(
+    path.join(base, "schema.schema.json")
+  );
+  const ajv = new Ajv({ loadSchema });
 
-  ajv.compileAsync(
-    schemaSchema
-  ).then((validate) => {
-    const valid = validate(
-      {
-        "tables": [
-          {
-            "name": "users",
-            "columns": [
-              {
-                "name": "firstName",
-                "type": "float",
-                scale: 'something'
-              }
-            ]
-          }
-        ]
-      }
-    )
-    if(!valid) {
-      console.log('Error!');
+  ajv.compileAsync(schemaSchema).then(validate => {
+    const valid = validate({
+      tables: [
+        {
+          name: "users",
+          columns: [
+            {
+              name: "firstName",
+              type: "float",
+              scale: "something"
+            }
+          ]
+        }
+      ]
+    });
+    if (!valid) {
+      console.log("Error!");
     } else {
-      console.log('Valid!');
+      console.log("Valid!");
     }
-  })
+  });
 }
 
-go()
+go();
