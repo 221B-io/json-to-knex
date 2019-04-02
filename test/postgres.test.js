d3599070d4ac1a69d3661dfb33251c54676fe638
Cameron Blandford <cam.blandford@gmail.com>
2019-04-02 13:40:12 -0400

    Show postgres working

--- test/postgres.test.js
+++ test/postgres.test.js
@@ -4,34 +4,96 @@ const _ = require("lodash");
 const Knex = require("knex");
 const fs = require("fs");
 
-const knexConfig = {
-  client: "sqlite3",
-  connection: {
-    filename: "./test.sqlite3"
+let config = {
+  knex: {
+    // just the usual knex configuration
+    client: "postgres",
+    connection: {
+      host: "localhost",
+      database: "jtk-test",
+      user: "pgtestuser"
+      // password: "pgtestpassword"
+    },
+    pool: {
+      min: 0,
+      max: 10
+    }
+    // migrations: {
+    //   directory: __dirname + "/migrations"
+    // }
   },
-  useNullAsDefault: true,
-  debug: true
+  dbManager: {
+    // db manager related configuration
+    collate: ["fi_FI.UTF-8", "Finnish_Finland.1252"],
+    superUser: "pgtestuser",
+    superPassword: "pgtestpassword"
+  }
 };
 
 let knex;
 
 beforeAll(async () => {
-  knex = Knex(knexConfig);
+  knex = Knex(config.knex);
 });
 
 afterAll(async () => {
   knex.destroy();
-  fs.unlink("./test.sqlite3", err => {
-    if (err) throw err;
-    console.log("test database successfully deleted");
-  });
+  // fs.unlink("./test.sqlite3", err => {
+  //   if (err) throw err;
+  //   console.log("test database successfully deleted");
+  // });
 });
 
 beforeEach(async () => {
-  await builder.dropTablesIfExists(knex, schema);
+  // await builder.dropTablesIfExists(knex, schema);
 });
 
 afterEach(async () => {
-  await builder.dropTablesIfExists(knex, schema);
-  await knex.schema.dropTableIfExists("users");
+  // await builder.dropTablesIfExists(knex, schema);
+  await knex.schema.dropTableIfExists("books");
+});
+
+const bookSchema = {
+  columns: [
+    {
+      name: "id",
+      type: "increments"
+    },
+    {
+      name: "title",
+      type: "string"
+    },
+    {
+      name: "author",
+      type: "string"
+    }
+  ]
+};
+let bookColsExpected = {
+  id: {
+    type: "integer",
+    maxLength: null,
+    nullable: false,
+    defaultValue: "nextval('books_id_seq'::regclass)"
+  },
+  title: {
+    type: "character varying",
+    maxLength: 255,
+    nullable: true,
+    defaultValue: null
+  },
+  author: {
+    type: "character varying",
+    maxLength: 255,
+    nullable: true,
+    defaultValue: null
+  }
+};
+test("create a single table", async () => {
+  await builder.createTable(knex.schema, "books", bookSchema);
+  let bookCols = await knex("books").columnInfo();
+  console.log(JSON.stringify(bookCols, null, 2));
+  expect(JSON.stringify(bookCols, null, 2)).toEqual(
+    JSON.stringify(bookColsExpected, null, 2)
+  );
 });
