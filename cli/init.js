ab4c6a3e8620d5a83dcf6df88b49d27912364e63
Cameron Blandford <cam.blandford@gmail.com>
2019-04-08 14:05:28 -0400

    Add cli stubs

---
+++ cli/init.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const migrator = require("../lib/migrations");
+const dbManager = require("../lib/db-manager");
+// check for dbSchema.json
+
+(async () => {
+  await dbManager.makeMigrationsTable();
+})();
