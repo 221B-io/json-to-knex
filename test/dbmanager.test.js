c08e9dea4ac23471d3692f22467bc86b0f9bf00f
Cameron Blandford <cam.blandford@gmail.com>
2019-04-09 13:32:18 -0400

    Add full, passing tests of functionality with postgres

--- test/dbmanager.test.js
+++ test/dbmanager.test.js
@@ -1,7 +1,7 @@
 const Manager = require("../lib/db-manager");
 const Knex = require("knex");
 
-const knexConfig = {
+const knexPostgresConfig = {
   // just the usual knex configuration
   client: "postgres",
   connection: {
@@ -19,5 +19,144 @@ const knexConfig = {
   // }
 };
 
-const knex = Knex(knexConfig);
-const manager = Manager(knex);
+const knexSqliteConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./test.sqlite3"
+  },
+  useNullAsDefault: true,
+  debug: true
+};
+
+const knex = Knex(knexPostgresConfig);
+const m = Manager(knex);
+
+beforeEach(async () => {
+  // drop all tables
+  await knex.schema.dropTableIfExists("books");
+  await knex.schema.dropTableIfExists("persons");
+});
+
+afterAll(async () => {
+  knex.destroy();
+});
+
+const schema1 = {
+  tables: [
+    {
+      name: "persons",
+      columns: [
+        { name: "firstName", type: "string" },
+        {
+          name: "age",
+          type: "integer"
+        },
+        {
+          name: "joined",
+          type: "datetime"
+        }
+      ]
+    }
+  ]
+};
+
+const schema2 = {
+  tables: [
+    {
+      name: "persons",
+      columns: [
+        { name: "id", type: "increments", primary: true },
+        { name: "firstName", type: "string" },
+        {
+          name: "age",
+          type: "integer"
+        },
+        {
+          name: "joined",
+          type: "datetime"
+        }
+      ]
+    },
+    {
+      name: "books",
+      columns: [
+        {
+          name: "title",
+          type: "string"
+        },
+        {
+          name: "authorId",
+          type: "integer",
+          unsigned: true
+        }
+        // { // TODO: add back in FK's
+        //   foreign: "authorId",
+        //   references: "id",
+        //   inTable: "persons",
+        //   onDelete: "CASCADE"
+        // }
+      ]
+    }
+  ]
+};
+
+const schema3 = {
+  tables: [
+    {
+      name: "persons",
+      columns: [
+        { name: "id", type: "increments", primary: true },
+        { name: "firstName", type: "string" },
+        {
+          name: "age",
+          type: "integer"
+        },
+        {
+          name: "joined",
+          type: "datetime"
+        }
+      ]
+    },
+    {
+      name: "books",
+      columns: [
+        {
+          name: "title",
+          type: "string"
+        },
+        {
+          name: "author",
+          type: "string"
+        }
+      ]
+    }
+  ]
+};
+
+async function getMigrationRows() {
+  return await knex("_migrations").select("*");
+}
+
+test("should do thing", async () => {
+  await m.makeMigrationsTable();
+  await m.addMigration(schema1);
+  await m.addMigration(schema2);
+  await m.addMigration(schema3);
+  await m.rollbackCurrentMigration();
+  await m.rollbackCurrentMigration();
+  await m.applyNextMigration();
+
+  console.log(await getMigrationRows());
+  expect(true);
+});
+
+const go = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+(async () => {
+  await go();
+})();
